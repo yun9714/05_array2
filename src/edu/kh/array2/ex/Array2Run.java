6e81f1e52931375c04302a77e7ed8ed2efdd3ace
HYUNGYUN HEO <yunhh9714@gmail.com>
2022-12-14 14:05:03 +0900

    12.14 수 first commit

---
+++ src/edu/kh/array2/ex/Array2Run.java
@@ -0,0 +1,11 @@
+package edu.kh.array2.ex;
+
+public class Array2Run {
+	public static void main(String[] args) {
+		Array2Example ae = new Array2Example();
+		ae.ex1();
+		ae.ex2();
+		ae.ex3();
+		
+	}
+}
